79a542806dc813b13cba10174a63afac24560911
zcl <838407200@qq.com>
2025-11-27 09:08:36 +0800

    add database support with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
+import os, sys
+from email.policy import default
+from pathlib import Path
+
 from flask import Flask, render_template, request
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' if sys.platform.startswith('win') else 'sqlite:////'
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI + str(Path(app.root_path) / 'data.db')
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(app, model_class=Base)
+
 
 @app.route('/hello')
 def hello():
     return '<h1>Hello World!</h1><img src="http://helloflask.com/totoro.gif">'
 
+
 @app.route('/user/<name>')
 def user_page(name):
     return f'User {escape(name)}'
-
-
 
 
 name = 'Grey Li'
@@ -30,5 +46,62 @@ movies = [
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = db.session.execute(select(User)).scalar()
+    movies = db.session.execute(select(Movie)).scalars().all()
+    return render_template('index.html', user=user, movies=movies)
 
+
+from sqlalchemy import String, select
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(60))
+    year: Mapped[str] = mapped_column(String(4))
+
+
+import click
+
+
+@app.cli.command('init-db')
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def init_database(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized the database.')
+
+@app.cli.command()
+def forge():
+    db.drop_all()
+    db.create_all()
+
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user=User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie=  Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done!')
